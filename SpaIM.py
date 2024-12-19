a89a52819dddf82d1f737534339c3f0018f93824
Bo_li <83391363+Boom5426@users.noreply.github.com>
2024-12-19 15:09:52 +0800

    Update SpaIM.py

--- SpaIM.py
+++ SpaIM.py
@@ -10,7 +10,6 @@ def gram_matrix(feat):
     G = torch.mm(feat, feat.t()) # b * d * d * b
     return G.div(b * d)
 
-
 class mlp_simple(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super().__init__()
